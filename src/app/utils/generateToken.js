6781ac7e683e89cd214ccdd156ee85c52ebc369a
phuc165 <hatanphuc3456789@gmail.com>
2025-06-09 17:46:40 +0700

    fix: update SameSite attribute for JWT cookie to 'lax' for improved security

--- src/app/utils/generateToken.js
+++ src/app/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-        sameSite: 'none',
+        sameSite: 'lax', // Changed from 'none' to 'lax'
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 };
